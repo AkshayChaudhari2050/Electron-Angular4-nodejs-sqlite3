4ce41ae4936281f7945b42f7cc03c81efc33c565
akshay chodhri <akshay.chaudhari@balajiinfosol.com>
2017-11-22 16:24:15 +0530

    app folder

--- src/student/student.ts
+++ src/student/student.ts
@@ -1,7 +1,7 @@
 export class Student {
-    studentid: number
-    grno: number
-    studentName: string
-    Std: string
-    Div: string
+    studentid: number;
+    grno: number;
+    studentName: string;
+    Std: string;
+    Div: string;
 } 
