0c3ba8fec999c5c4d6b254e964b136e1a2b422ff
akshay chodhri <akshay.chaudhari@balajiinfosol.com>
2017-11-22 17:01:37 +0530

    last update

---
+++ src/app/hellocomponent/hellocomponent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hellocomponent',
+  templateUrl: './hellocomponent.component.html',
+  styleUrls: ['./hellocomponent.component.scss']
+})
+export class HellocomponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
